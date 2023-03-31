3d8ba7f9ca54f9d22b7f42433c6e322de0cce7af
MGisonni <massimo.gisonni@gmail.com>
2023-03-31 13:57:41 +0000

    small changes

--- src/energies.c
+++ src/energies.c
@@ -4,7 +4,7 @@
 void ekin(mdsys_t *sys)
 {
     int i;
-
+    
     sys->ekin=0.0;
     for (i=0; i<sys->natoms; ++i) {
         sys->ekin += 0.5*mvsq2e*sys->mass*(sys->vx[i]*sys->vx[i] + sys->vy[i]*sys->vy[i] + sys->vz[i]*sys->vz[i]);
